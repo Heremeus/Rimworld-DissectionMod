bb5925001e94e9a3a1756dbe9b359dcab0f884ee
Armin Willer <armin_willer@yahoo.de>
2018-03-01 21:46:25 +0000

    Initial commit

---
+++ Source/HMDissection/CompExpHandler.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace HMDissection
+{
+    public class CompExpHandler : ThingComp
+    {
+        public CompProperties_ExpHandler Props
+        {
+            get => (CompProperties_ExpHandler)props;
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            
+            if (!parent.Spawned)
+            {
+                return;
+            }
+            if (parent.TryGetComp<CompPowerTrader>() != null && !parent.TryGetComp<CompPowerTrader>().PowerOn)
+            {
+                return;
+            }
+            
+            List<Thing> affectedThings = (parent.InteractionCell.GetThingList(parent.Map));
+            
+            foreach (Thing t in affectedThings)
+            {
+                Pawn p = t as Pawn;
+                if(p != null)
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Source/HMDissection/CompProperties_ExpHandler.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace HMDissection
+{
+    public class CompProperties_ExpHandler : CompProperties
+    {
+        public int expAmount;
+
+        public CompProperties_ExpHandler()
+        {
+            compClass = typeof(CompExpHandler);
+        }
+    }
+}
